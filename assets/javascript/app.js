0aa206b49291016bd8a4fe0fbf73c6be8ed2dca7
Lauren <lwfriel@gmail.com>
2019-07-09 16:40:02 -0500

    added HTML, created IDs, psedocoded JavaScript

---
+++ assets/javascript/app.js
@@ -0,0 +1,25 @@
+$(document).ready(function () {
+//Create start button
+//Create Click for start button 
+//Create timer for "time remaining"
+//Show first question
+//Show 4 answers
+//Create Clicks for 4 answers 
+//Create correct and incorrect 
+//Promt if answer was correct or incorrect & display correct answer
+//If ran out of time - show correct answer 
+//Move to next question
+//Restart Timer 
+//Show Question 2 
+//Show Answers
+////Create Clicks for 4 answers 
+//Create correct and incorrect 
+//Promt if answer was correct or incorrect & display correct answer
+//If ran out of time - show correct answer
+//Next question - repeat steps for questions 
+//Final Page Show time remaining ("All done, here is how you did" )
+
+
+
+
+})
