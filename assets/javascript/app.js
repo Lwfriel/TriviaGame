32e94a2e0cf2eab0d96926e80aa3c9b535dbd883
Lauren <lwfriel@gmail.com>
2019-07-09 20:41:13 -0500

    created logic for answers and if else

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -45,13 +45,15 @@ $(document).ready(function () {
 
     var intervalId;
 
+    var clickDisable = false;
+
     function stop() {
         clearInterval(intervalId);
     }
 
     $("#gameBox").hide()
     $("#score").hide();
-    
+
 
 
     function timer() {
@@ -59,9 +61,9 @@ $(document).ready(function () {
         intervalId = setInterval(function () {
             timeLeft--;
             $("#timerBox").html("<h2> Time Remaining: " + timeLeft + "</h2>");
-            if (timeLeft === 0){
+            if (timeLeft === 0) {
                 stop();
-                $("#rightAnswer").html("<h2>The Correct Answer is: " + questionArray[count].ans +  "</h2>")
+                $("#rightAnswer").html("<h2>The Correct Answer is: " + questionArray[count].ans + "</h2>")
                 unanswered++
                 timeout()
             }
@@ -69,16 +71,17 @@ $(document).ready(function () {
     }
 
     function timeout() {
-        setTimeout(function(){
+        setTimeout(function () {
+            clickDisable = false;
             count++;
             nextQuestion();
-      }, 5000);
+        }, 5000);
 
 
     }
 
     function nextQuestion() {
-        if (count===questionArray.length){
+        if (count === questionArray.length) {
             $("#score").show();
             $("#gameBox").hide();
             $("#correct").text(correct);
@@ -86,8 +89,8 @@ $(document).ready(function () {
             $("#unanswered").text(unanswered);
             return
         }
-        timeLeft = 3; 
-        timer(); 
+        timeLeft = 10;
+        timer();
         questionDOM();
 
     }
@@ -113,40 +116,121 @@ $(document).ready(function () {
         nextQuestion();
     }
 
-   
-
     $("#startButton").click(play)
 
+    $("#a").click(function () {
+        if (clickDisable === true) {
+            return
+        }
+        clickDisable = true;
+        stop()
+        if (questionArray[count].a === questionArray[count].ans) {
+            correct++;
+            $("#isCorrect").html("<h1>You are correct!</h1>");
+            $("#rightAnswer").html("<h1>The correct answer is:" + questionArray[count].ans + "</h1>");
+            timeout()
+        }
+        else {
+            incorrect++;
+            $("#isCorrect").html("<h1>You are incorrect!</h1>");
+            $("#rightAnswer").html("<h1>The correct answer is:" + questionArray[count].ans + "</h1>");
+            timeout()
+        }
+
+    }
+    )
+    $("#b").click(function () {
+        if (clickDisable === true) {
+            return
+        }
+        clickDisable = true;
+        stop()
+        if (questionArray[count].b === questionArray[count].ans) {
+            correct++;
+            $("#isCorrect").html("<h1>You are correct!</h1>");
+            $("#rightAnswer").html("<h1>The correct answer is:" + questionArray[count].ans + "</h1>");
+            timeout()
+        }
+        else {
+            incorrect++;
+            $("#isCorrect").html("<h1>You are incorrect!</h1>");
+            $("#rightAnswer").html("<h1>The correct answer is:" + questionArray[count].ans + "</h1>");
+            timeout()
+        }
+
+    }
+    )
+
+    $("#c").click(function() {
+        if (clickDisable === true) {
+            return
+        }
+        clickDisable = true;
+        stop()
+        if (questionArray[count].c===questionArray[count].ans){ 
+            correct++; 
+            $("#isCorrect").html("<h1>You are correct!</h1>");
+            $("#rightAnswer").html("<h1>The correct answer is:" + questionArray[count].ans + "</h1>");
+            timeout()
+        }
+        else {
+        incorrect++; 
+        $("#isCorrect").html("<h1>You are incorrect!</h1>");
+        $("#rightAnswer").html("<h1>The correct answer is:" + questionArray[count].ans + "</h1>");
+        timeout()
+        }
+
+    }
+)
+
+$("#d").click(function() {
+    if (clickDisable === true) {
+        return
+    }
+    clickDisable = true;
+    stop()
+    if (questionArray[count].d===questionArray[count].ans){ 
+        correct++; 
+        $("#isCorrect").html("<h1>You are correct!</h1>");
+        $("#rightAnswer").html("<h1>The correct answer is:" + questionArray[count].ans + "</h1>");
+        timeout()
+    }
+    else {
+    incorrect++; 
+    $("#isCorrect").html("<h1>You are incorrect!</h1>");
+    $("#rightAnswer").html("<h1>The correct answer is:" + questionArray[count].ans + "</h1>");
+    timeout()
+    }
+
+}
+)
 
 
-
-
-
-    //Create start button
-    //Create Click for start button (Use .click)
-    //Hide Start Button (.hide function Jquery/MDN)
-    //Create timer for "time remaining" (use setInterval)
-    //Show first question (Use ".show" function MDN/Jquery on gamebox)
-    //Show 4 answers 
-    //Create Clicks for 4 answers (Use .click)
+    //Create start button * 
+    //Create Click for start button (Use .click) * 
+    //Hide Start Button (.hide function Jquery/MDN) * 
+    //Create timer for "time remaining" (use setInterval) * 
+    //Show first question (Use ".show" function MDN/Jquery on gamebox) * 
+    //Show 4 answers * 
+    //Create Clicks for 4 answers (Use .click)  
     //Create correct and incorrect (if/else)
     //Promt if answer was correct or incorrect & display correct answer- show correct answer (variables for correct/incorrect/unanswered (increases))
     //Create var for scores (increases), timer (decreases), arrays 
     //If ran out of time - show correct answer (.show function on displayBox)
     //Timeout for 3 second after each answer shown (use setTimeout)
-    //Move to next question
-    //Restart Timer  
-    //Show Question 2 (repeat function - Pull next question from array) 
-    //Show Answers 
+    //Move to next question *
+    //Restart Timer  *
+    //Show Question 2 (repeat function - Pull next question from array) *
+    //Show Answers *
     ////Create Clicks for 4 answers 
     //Create correct and incorrect 
     //Promt if answer was correct or incorrect & display correct answer
     //If ran out of time - show correct answer
-    //Timeout for 3 second after each answer shown 
-    //Next question - repeat steps for questions 
-    //Hide gameBox
-    //Show scoreBox
-    //Final Page Show time remaining ("All done, here is how you did" )
+    //Timeout for 3 second after each answer shown *
+    //Next question - repeat steps for questions * 
+    //Hide gameBox * 
+    //Show scoreBox * 
+    //Final Page Show time remaining ("All done, here is how you did" ) * 
     //Create Startover button with reset for game 
 
 
